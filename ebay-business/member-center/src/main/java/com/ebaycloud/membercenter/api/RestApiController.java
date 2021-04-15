5f9a0f8c6f117b43c47798d9f9897df521ebc584
yanchao00551 <10947@163.com>
2021-04-15 16:57:19 +0800

    up

---
+++ ebay-business/member-center/src/main/java/com/ebaycloud/membercenter/api/RestApiController.java
@@ -0,0 +1,23 @@
+package com.ebaycloud.membercenter.api;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @PackageName:com.ebaycloud.membercenter.api
+ * @ClassName:RestController
+ * @Description:
+ * @author: 悟空
+ * @date: 2021/4/15 16:43
+ * @email: 10947@163.com
+ */
+@RestController
+@RequestMapping(value = "/member")
+public class RestApiController {
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    public String getUserById(@PathVariable String id){
+        return "user: " + id;
+    }
+}
