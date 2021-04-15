f6f4bdead8a6c30aa8cd6571ec600a54f1ecab7f
yanchao00551 <10947@163.com>
2021-04-15 16:34:22 +0800

    up

--- ebay-zuul-gateway/src/main/java/com/ebaycloud/ebayzuulgateway/EbayZuulGatewayApplication.java
+++ ebay-zuul-gateway/src/main/java/com/ebaycloud/ebayzuulgateway/EbayZuulGatewayApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 @SpringBootApplication
 public class EbayZuulGatewayApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EbayZuulGatewayApplication.class, args);
     }
-
 }
